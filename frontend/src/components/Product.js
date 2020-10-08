280e4813caeb428e0bc57639dbd2f283855f67ca
Nico Yeager <nicoyeager@gmail.com>
2020-10-07 21:37:30 -0400

    Implemented rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 py-3 rounded">
@@ -12,8 +14,11 @@ const Product = ({ product }) => {
             <a href={`/products/${product._id}`}>{product.name}</a>
           </strong>
         </Card.Title>
-        <Card.Text>
-          {product.rating} from {product.numReviews} reviews
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as="h4">${product.price}</Card.Text>
       </Card.Body>
